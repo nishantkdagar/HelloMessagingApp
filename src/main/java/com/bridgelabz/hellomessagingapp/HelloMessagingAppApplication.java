135ee7bdd13dcfb982e66eee9660db0a1ec5457d
Nishan Kumar <nishantkumarkd@gmail.com>
2022-08-29 07:02:02 +0530

    Creating Spring App

--- src/main/java/com/bridgelabz/hellomessagingapp/HelloMessagingAppApplication.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/HelloMessagingAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloMessagingAppApplication {
 
     public static void main(String[] args) {
-        System.out.println("Setting up the Project");
         SpringApplication.run(HelloMessagingAppApplication.class, args);
     }
 
